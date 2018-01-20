11cdbb007fe1969ef3a93e1908403aad635516dd
Yury Kozhenov <yurykozhenov@gmail.com>
2018-01-20 05:03:44 +0200

    Fix typescript error

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './modules/app.module';
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
