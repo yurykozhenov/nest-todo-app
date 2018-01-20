b0eb494e26bc76097234036275de9bf262d92652
Yury Kozhenov <yurykozhenov@gmail.com>
2018-01-20 05:02:07 +0200

    Add customizable port from env

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './modules/app.module';
 
+const PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 
 bootstrap();
